7540ea01d61093f306e7a1c3fb68360817bb02b7
Matt Crinklaw-Vogt <matt.crinklaw@gmail.com>
2013-10-12 17:12:14 -0400

    Renamed component.json to bower.json, added bootstrap to bower.json

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,25 +1,24 @@
 'use strict';
 
 module.exports = function (grunt) {
-    // load all grunt tasks
-    require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+	require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
 
 	grunt.initConfig({
 		concat: {
-            options: {
-                seperator: ';'
-            },
-            dist: {
-                src: ['src/header.js',
-                      'src/impls/utils.js',
-                      'src/impls/FilesystemAPIProvider.js',
-                      'src/impls/IndexedDBProvider.js',
-                      'src/impls/LocalStorageProvider.js',
-                      'src/impls/WebSQLProvider.js',
-                      'src/LargeLocalStorage.js',
-                      'src/footer.js'],
-                dest: 'dist/LargeLocalStorage.js'
-            }
-        }
-    });
+			options: {
+				seperator: ';'
+			},
+			dist: {
+				src: ['src/header.js',
+					  'src/impls/utils.js',
+					  'src/impls/FilesystemAPIProvider.js',
+					  'src/impls/IndexedDBProvider.js',
+					  'src/impls/LocalStorageProvider.js',
+					  'src/impls/WebSQLProvider.js',
+					  'src/LargeLocalStorage.js',
+					  'src/footer.js'],
+				dest: 'dist/LargeLocalStorage.js'
+			}
+		}
+	});
 };
